b5ce0eb15a05365fa9d03ea35a4b127f75f8720f
Tom McLeod <tom.mcleod@opalkelly.com>
2018-08-08 14:41:29 -0700

    Python script updates
    Update the python script to use floating point data directly, performing
    bit shifts as necessary to match the Xilinx fixed point settings.
    
    Also calculate the total error against the reference waveform accumulated
    through running to determine if the HLS module has performed as expected.

--- Python/FP-HLS.py
+++ Python/FP-HLS.py
@@ -8,6 +8,11 @@ PIPE_OUT_EP = 0xA0
 RESET_EP = 0x00
 TRIGGER_EP = 0x40
 
+def twos_comp(val, bits):
+    if (val & (1 << (bits - 1))):
+        val = val - (1 << bits)
+    return val
+
 def pipe_write(buf):
     ret = dev.WriteToPipeIn(PIPE_IN_EP, buf)
 
@@ -23,7 +28,12 @@ def pipe_read(length):
     dev.ReadFromPipeOut(PIPE_OUT_EP, output_buf)
     n = 8
     """ List containing output """
-    out_arr = [int.from_bytes(output_buf[i:i + n], byteorder = "little") for i in range(0, len(output_buf), n)]
+    out_arr = []
+    for i in range (0, len(output_buf), n):
+        temp_var = int.from_bytes(output_buf[i:i+n], byteorder = "little")
+        # Convert output data back to a float, shift right by 36 (divide by 2^36)
+        temp_var = float(twos_comp(temp_var, 48)) / (2**36)
+        out_arr.append(temp_var)
     return out_arr
 
 def transaction(in_buf, out_arr):
@@ -33,6 +43,11 @@ def transaction(in_buf, out_arr):
     out_arr.extend(pipe_read(len(in_buf) * 2))
 
 """ Open device """
+if (len(sys.argv) < 5):
+    print("Usage:")
+    print("python3 FP-HLS.py <bitstream> <infile> <reference output> <outfile>")
+    quit()
+
 dev = ok.okCFrontPanel()
 if (dev.NoError != dev.OpenBySerial("")):
 	print("A device could not be opened.")
@@ -46,13 +61,19 @@ dev.UpdateWireIns()
 dev.SetWireInValue(RESET_EP, 0x0)
 dev.UpdateWireIns()
 
+input_float = []
 input_buf = bytearray()
 output_arr = []
 with open(sys.argv[2]) as infile:
     i = 0
     for line in infile:
         line = line.rstrip()
-        val = int(line).to_bytes(4, byteorder = "little")
+        input_float.append(float(line))
+        # Convert float data to bytes, shift left by 16 (multiply by 2^16) to
+        # match fixed point settings in HLS
+        val = int(float(line) * (2**16)).to_bytes(4, byteorder = "little", signed=True)
+        #if i < 10:
+            #print((float(line) * (2**16)))
         input_buf.extend(val)
         if i == 1023:
             transaction(input_buf, output_arr)
@@ -65,6 +86,20 @@ while len(input_buf) != 4096:
     input_buf.append(0x0)
 
 transaction(input_buf, output_arr)
+
+total_error = 0
+
+with open(sys.argv[3]) as reffile:
+    for i, line in enumerate(reffile):
+        line = line.rstrip()
+        ref_val = float(line)
+        error = output_arr[i] - ref_val
+        total_error += error
+
+if (total_error < 10.0):
+    print("Success! Error within bounds!")
+else:
+    print("Error out of bounds")
 
 with open(sys.argv[4], mode = "wt") as outfile:
     outfile.write('\n'.join(str(value) for value in output_arr))
