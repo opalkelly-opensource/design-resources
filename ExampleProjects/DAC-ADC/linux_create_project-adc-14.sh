1cbb4a3f678ee012da21857200816144c0d34d23
hayhut <67342286+hayhut@users.noreply.github.com>
2024-07-09 11:45:14 -0700

    reorder file structure for dac-adc (#75)

---
+++ ExampleProjects/DAC-ADC/linux_create_project-adc-14.sh
@@ -0,0 +1,16 @@
+# For instructions on how to execute this build script, see 'How-To Setup the Project'
+# at the Spectrum Analyzer sample's documentation located at the link provided
+# in this sample's README.md.
+
+if [ $# -eq 0 ]
+   then
+      echo "Usage: ./linux_create_project.sh [path to FrontPanel Vivado IP]"
+      exit 1
+fi
+
+cd gateware/adc-14/ifft
+vitis_hls build_ifft_vitis.tcl
+cd ../fft
+vitis_hls build_fft_vitis.tcl
+cd ..
+vivado -source create_project_vivado.tcl -tclargs $1
