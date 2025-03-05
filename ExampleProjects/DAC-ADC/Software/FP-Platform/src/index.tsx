1ea98265cdcc3fc90d661d27442f7f2b64094db3
hayhut <67342286+hayhut@users.noreply.github.com>
2025-03-04 18:26:58 -0800

    Add updated DAC-ADC example
    Uses FrontPanel 6 GUI assets

---
+++ ExampleProjects/DAC-ADC/Software/FP-Platform/src/index.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2024-2025 Opal Kelly Incorporated
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+//TODO: Use the correct bitfile for your ADC
+//import "../assets/DAC-ADC-ExampleDesign-ADC-12-v1.0.bit";
+import "../assets/DAC-ADC-ExampleDesign-ADC-14-v1.0.bit";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
